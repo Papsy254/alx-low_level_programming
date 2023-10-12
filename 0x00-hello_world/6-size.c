bbbc060775d895bb0aaa49e6d7683ee44ff50838
Papsy254 <immanuelmureithi325@gmail.com>
2023-10-12 12:13:29 -0700

    6-size.c
    git commit -m 6-size.c

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,10 +1,11 @@
 #include <stdio.h>
 /**
- * main - A program that prints the size of various computer types 
- * Return: 0 (Success)
+ * main - A program that prints the size of various types on the computer it is compiled and run on.
+ * Return 0 (Success)
  */
 int main(void)
 {
+
 	char a;
 	int b;
 	long int c;
