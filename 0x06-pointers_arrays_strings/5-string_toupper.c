1756df6c8931d8f76293e2be3b14d7db4c0e4751
Papsy254 <immanuelmureithi325@gmail.com>
2023-10-19 06:36:34 -0700

    5-string_toupper.c

--- 0x06-pointers_arrays_strings/5-string_toupper.c
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -1,8 +1,8 @@
 #include "main.h"
 /**
- *  string_toupper - changes all lowercase to uppercase
- *  @n: pointer
- *  Return: n
+ * string_toupper - change all lowercase to uppercase
+ * @n: pointer
+ * Return: n
  */
 char *string_toupper(char *n)
 {
