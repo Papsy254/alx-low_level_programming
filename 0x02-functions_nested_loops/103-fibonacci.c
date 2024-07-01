28d6e2650bebeb5d50edcff69d55a9c8b3277c3b
Papsy254 <immanuelmureithi325@gmail.com>
2024-07-01 07:22:11 -0700

    103-fibonacci.c

--- 0x02-functions_nested_loops/103-fibonacci.c
+++ 0x02-functions_nested_loops/103-fibonacci.c
@@ -10,7 +10,7 @@ int main(void)
 	long int first = 1, second = 2, next;
 	long int sum = 0;
 
-	while (second <= 4000000)	
+	while (second <= 4000000)
 	{
 	if (second % 2 == 0)
 	{
@@ -23,5 +23,5 @@ int main(void)
 
 	printf("%ld\n", sum);
 
-	return 0;
+	return (0);
 }
