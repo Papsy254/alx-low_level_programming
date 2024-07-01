3fd3886222e1d5388591da4eed0ee84c5dd05a37
Papsy254 <immanuelmureithi325@gmail.com>
2024-07-01 07:19:47 -0700

    103-fibonacci.c

---
+++ 0x02-functions_nested_loops/103-fibonacci.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point of the program
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	long int first = 1, second = 2, next;
+	long int sum = 0;
+
+	while (second <= 4000000)	
+	{
+	if (second % 2 == 0)
+	{
+	sum += second;
+	}
+	next = first + second;
+	first = second;
+	second = next;
+	}
+
+	printf("%ld\n", sum);
+
+	return 0;
+}
