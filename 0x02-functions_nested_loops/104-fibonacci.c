4599e5781edfa6e7f87b3573ec34008f3e21f415
Papsy254 <immanuelmureithi325@gmail.com>
2024-07-02 08:32:16 -0700

    104-fibonacci.c

--- 0x02-functions_nested_loops/104-fibonacci.c
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -1,5 +1,12 @@
 #include <stdio.h>
 
+void print_fibonacci(unsigned long int first_high,
+		unsigned long int first_low,
+		unsigned long int second_high,
+		unsigned long int second_low);
+void print_number(unsigned long int high,
+		unsigned long int low);
+
 /**
  * main - Entry point of the program
  *
@@ -7,15 +14,34 @@
  */
 int main(void)
 {
-	unsigned long int first_high, first_low, second_high, second_low, next_high, next_low;
-	int i;
-	
-	first_high = 0;
-	first_low = 1;
-	second_high = 0;
-	second_low = 2;
+	unsigned long int first_high = 0;
+	unsigned long int first_low = 1;
+	unsigned long int second_high = 0;
+	unsigned long int second_low = 2;
 
 	printf("%lu, %lu, ", first_low, second_low);
+
+	print_fibonacci(first_high, first_low,
+			second_high, second_low);
+
+	printf("\n");
+	return (0);
+}
+
+/**
+ * print_fibonacci - Prints the next 96 Fibonacci numbers
+ * @first_high: High part of the first Fibonacci number
+ * @first_low: Low part of the first Fibonacci number
+ * @second_high: High part of the second Fibonacci number
+ * @second_low: Low part of the second Fibonacci number
+ */
+void print_fibonacci(unsigned long int first_high,
+		unsigned long int first_low,
+		unsigned long int second_high,
+		unsigned long int second_low)
+{
+	unsigned long int next_high, next_low;
+	int i;
 
 	for (i = 3; i <= 98; i++)
 	{
@@ -30,26 +56,35 @@ int main(void)
 	next_high = first_high + second_high;
 	}
 
-	first_high = second_high;
-	first_low = second_low;
-	second_high = next_high;
-	second_low = next_low;
-
-	if (next_high == 0)
-	{
-	printf("%lu", next_low);
-	}
-	else
-	{
-	printf("%lu%010lu", next_high, next_low);
-	}
+	print_number(next_high, next_low);
 
 	if (i != 98)
 	{
 	printf(", ");
 	}
-	}
 
-	printf("\n");
-	return 0;
+	first_high = second_high;
+	first_low = second_low;
+	second_high = next_high;
+	second_low = next_low;
+	}
+}
+
+/**
+ * print_number - Prints a Fibonacci number with high and low parts
+ * @high: High part of the number
+ * @low: Low part of the number
+ */
+void print_number(unsigned long int high,
+		unsigned long int low)
+{
+	if (high > 0)
+	{
+	printf("%lu%010lu", high, low);
+	}
+	else
+	{
+	printf("%lu", low);
+
+	}
 }
