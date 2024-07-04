d08831533288946c160ba445e2d793fd76940cbc
Papsy254 <immanuelmureithi325@gmail.com>
2024-07-04 15:01:50 -0700

    104-main.c

--- 0x06-pointers_arrays_strings/104-print_buffer.c
+++ 0x06-pointers_arrays_strings/104-print_buffer.c
@@ -1,6 +1,43 @@
-#include "main.h"
+#include <stdio.h>
+#include <ctype.h>
+
 /**
- *  _strcat - concatenates two strings
- *  @dest: input value
- *  @src : input value
- *  Return: 
+ * print_buffer - Prints the content of a buffer in a specified format
+ * @b: Buffer to print
+ * @size: Size of the buffer
+ */
+void print_buffer(char *b, int size)
+{
+	int offset, i, j;
+
+	if (size <= 0)
+	{
+	printf("\n");
+	return;
+	}
+
+	for (offset = 0; offset < size; offset += 10)
+	{
+	printf("%08x: ", offset);
+
+	for (i = 0; i < 10; i++)
+	{
+	if (offset + i < size)
+	printf("%02x", (unsigned char)b[offset + i]);
+	else
+	printf("  ");
+	if (i % 2)
+	printf(" ");
+	}
+
+	for (j = 0; j < 10 && offset + j < size; j++)
+	{
+	if (isprint((unsigned char)b[offset + j]))
+	printf("%c", b[offset + j]);
+	else
+	printf(".");
+	}
+
+	printf("\n");
+	}
+}
