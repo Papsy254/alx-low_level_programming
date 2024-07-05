7e63754a44313b94251b8e669646eb4d65783c62
Papsy254 <immanuelmureithi325@gmail.com>
2024-07-05 00:08:55 -0700

    ./100-atoi

---
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -0,0 +1,29 @@
+#include "main.h"
+
+/**
+ * _atoi - converts a string to an integer
+ * @s: the string to be converted
+ *
+ * Return: the integer converted from the string
+ */
+int _atoi(char *s)
+{
+	int result = 0;
+	int sign = 1;
+	int i = 0;
+
+	while (s[i] != '\0' && (s[i] < '0' || s[i] > '9'))
+	{
+	if (s[i] == '-')
+	sign = -sign;
+	i++;
+	}
+
+	while (s[i] >= '0' && s[i] <= '9')
+	{
+	result = result * 10 + (s[i] - '0');
+	i++;
+	}
+
+	return (result * sign);
+}
--- 0x05-pointers_arrays_strings/main.h
+++ 0x05-pointers_arrays_strings/main.h
@@ -8,5 +8,6 @@ void print_array(int *a, int n);
 void rev_string(char *s);
 void puts2(char *str);
 int _putchar(char c);
+int _atoi(char *s);
 
 #endif /* MAIN_H */
