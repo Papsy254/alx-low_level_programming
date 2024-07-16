aa951ed7d7be0461b219642f0112b298c2e46f85
Papsy254 <immanuelmureithi325@gmail.com>
2024-07-15 17:39:59 -0700

    update

---
+++ 0x09-static_libraries/functions.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/**
+ * print_message - Prints a message to the console
+ *
+ * Description: This function prints "Hello, World!" to the console.
+ * It does not take any parameters and does not return any value.
+ */
+
+void print_message(void)
+{
+	printf("Hello, World!\n");
+}
---
+++ 0x09-static_libraries/main.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point of the program
+ *
+ * Description: This function is the entry point of the program.
+ * It prints "Hello, World!" to the console and returns 0 to indicate
+ * successful execution.
+ *
+ * Return: Always 0 (Success)
+ */
+
+void print_message(void);
+
+int main(void)
+{
+	print_message();
+	return (0);
+}
