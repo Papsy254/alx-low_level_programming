a678dae91d245667927ee1725b34b1a3381311c4
Papsy254 <immanuelmureithi325@gmail.com>
2024-08-15 03:47:41 -0700

    update

--- 0x15-file_io/main.h
+++ 0x15-file_io/main.h
@@ -10,8 +10,9 @@
 
 int create_file(const char *filename, char *text_content);
 int append_text_to_file(const char *filename, char *text_content);
-
 ssize_t read_textfile(const char *filename, size_t letters);
+void close_file(int fd);
+void handle_error(const char *message, const char *filename, int exit_code);
 
 #endif /* MAIN_H */
 
